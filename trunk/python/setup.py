8560c04d9d78231d08a3c236cdd449216fe10de0
rcarlson.cs@gmail.com <rcarlson.cs@gmail.com@b14a1e29-0629-a4f1-dd69-b9b4775806f8>
2011-12-07 16:05:25 +0000

    Added distutils distribution system for installing G2P extension on a running system.

---
+++ trunk/python/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(name='g2p',
+      version='1.0',
+      description='Default Refine library Python extension',
+      author='Richard Carlson',
+      author_email='rcarlson.cs@gmail.com',
+      url='http://code.google.com/p/defaultrefine/',
+      packages=['g2p'],
+      )
