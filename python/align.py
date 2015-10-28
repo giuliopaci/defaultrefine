4fc437dff1c08a5980704afe9ee3a978f9436ea4
Matteo Lisotto <matteo.lisotto@gmail.com>
2015-10-28 11:11:00 +0100

    Add align.py in python folder

---
+++ python/align.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#-----------------------------------------------------------------------------#
+#                                                                             #
+# License:                                                                    #
+#                                                                             #
+#   Copyright (c) 2015 Mivoq Srl                                              #
+#                                                                             #
+#   All rights reserved.                                                      #
+#                                                                             #
+#   Contributors:                                                             #
+#                                                                             #
+#   Redistribution and use in source and binary forms, with or without        #
+#   modification, are permitted provided that the following conditions are    #
+#   met:                                                                      #
+#                                                                             #
+#     * Redistributions of source code must retain the above copyright        #
+#       notice, this list of conditions and the following disclaimer.         #
+#                                                                             #
+#     * Redistributions in binary form must reproduce the above copyright     #
+#       notice, this list of conditions and the following disclaimer in the   #
+#       documentation and/or other materials provided with the distribution.  #
+#                                                                             #
+#     * Neither the name of the Department of Arts and Culture nor the names  #
+#       of its contributors may be used to endorse or promote products        #
+#       derived from this software without specific prior written permission. #
+#                                                                             #
+#   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS       #
+#   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED #
+#   TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A           #
+#   PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER  #
+#   OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,  #
+#   EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,       #
+#   PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR        #
+#   PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF    #
+#   LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING      #
+#   NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS        #
+#   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.              #
+#                                                                             #
+#-----------------------------------------------------------------------------#
+
+import sys
+from g2p import *
+
+# Create the G2P
+g2p = G2P()
+dict = DictionaryFile().from_file(sys.argv[1])
+
+# Create the rules
+g2p.set_dictionary(dict)
+g2p.align()
+g2p.update_rules()
+rules = g2p.get_rules()
+
+# Print the rules
+print '\n'.join(rules)
